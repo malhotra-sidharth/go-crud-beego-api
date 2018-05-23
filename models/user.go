20e2fae87e4fdbbc4eb7864d55ce19bd62cea892
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-22 23:58:06 -0400

    [WIP] Added User Model

--- models/user.go
+++ models/user.go
@@ -1,1 +1,20 @@
 package models
+
+import (
+	"time"
+
+	"github.com/astaxie/beego/orm"
+)
+
+type User struct {
+	id        int
+	firstName string
+	lastName  string
+	email     string
+	password  string
+	regDate   time.Time
+}
+
+func init() {
+	orm.RegisterModel(new(User))
+}
