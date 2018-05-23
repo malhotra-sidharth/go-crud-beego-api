ce9486d2c2954de1a294cc935a8efe2aa6f5eb94
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-22 23:51:49 -0400

    [WIP] Added DB connection

--- main.go
+++ main.go
@@ -1,15 +1,49 @@
 package main
 
 import (
+	"fmt"
 	_ "go-crud-beego-api/routers"
+	"os"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	// if in develop mode
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	// get database configuration from environment variables
+	dbUser := os.Getenv("db_user")
+	dbPwd := os.Getenv("db_pwd")
+	dbName := "go_user_mgmt"
+	dbString := dbUser + ":" + dbPwd + "@/" + dbName + "?charset=utf8"
+
+	// Register Driver
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+
+	// Register default database
+	orm.RegisterDataBase("default", "mysql", dbString)
+
+	// autosync
+	// db alias
+	name := "default"
+
+	// drop table and re-create
+	force := true
+
+	// print log
+	verbose := true
+
+	// error
+	err := orm.RunSyncdb(name, force, verbose)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	beego.Run()
 }
